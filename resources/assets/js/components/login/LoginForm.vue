1f65d6108f0457f99526a6666413ec86e9ed37b0
meng-Derek <a0985265734@gmail.com>
2018-06-27 15:44:37 +0800

    add display not exit  email and password error meseage

--- resources/assets/js/components/login/LoginForm.vue
+++ resources/assets/js/components/login/LoginForm.vue
@@ -9,14 +9,14 @@
                 <span class="help-block" v-show="errors.has('email')">{{errors.first('email')}}</span>
             </div>
         </div>
-        <div class="form-group" :class="{'has-error': errors.has('password')}">
+        <div class="form-group" :class="{'has-error': errors.has('password') || bag.has('password:auth')}">
             <label for="password" class="col-md-3 control-label">密碼</label>
             <div class="col-md-7">
                 <input v-model="password"
                        v-validate data-vv-rules="required|min: 6" data-vv-as="密碼"
                        id="password" type="password" class="form-control" name="password" required>
                 <span class="help-block" v-show="errors.has('password')">{{errors.first('password')}}</span>
-
+                <span class="help-block" v-if="mismatchError">{{bag.first('password')}}</span>
             </div>
         </div>
         <div class="form-group">
@@ -31,12 +31,19 @@
 
 <script>
     import JWTToken from './../../helpers/jwt';
+    import {ErrorBag} from 'vee-validate';
 
     export default {
         data() {
             return {
                 email: '',
                 password: '',
+                bag: new ErrorBag()
+            }
+        },
+        computed: {
+            mismatchError() {
+                return this.bag.has('password:auth') && !this.errors.has('password')
             }
         },
         methods: {
@@ -49,6 +56,10 @@
                         }
                         this.$store.dispatch('loginRequest', formData).then(response => {
                             this.$router.push({name: 'profile'})
+                        }).catch(error => {
+                            if (error.response.status === 421) {
+                                this.bag.add('password', 'Email和密碼輸入錯誤', 'auth')
+                            }
                         })
                     }
                 })
--- resources/assets/js/store/modules/login.js
+++ resources/assets/js/store/modules/login.js
@@ -7,8 +7,6 @@ export default {
             return axios.post('/api/login', formData).then(response => {
                 jwtToken.setToken(response.data.token);
                 dispatch('setAuthUser')
-            }).catch(error => {
-                console.log(error.response);
             })
         },
         logoutRequest({dispatch}) {
